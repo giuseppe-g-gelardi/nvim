744520bed69a0c4df64b6f85b66d6c3acc9ad02c
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-03-17 11:35:51 -0400

    testing out diagnostics

--- lua/ggelardi/remap.lua
+++ lua/ggelardi/remap.lua
@@ -58,12 +58,11 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git); -- git status
 vim.keymap.set('n', '<leader>j', ':Neotree toggle<CR>', { noremap = true, silent = true }) -- toggle neo-tree
 
 
-
--- ! other diagnostic keymaps to try
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+-- *** other diagnostic keymaps to try
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 
 

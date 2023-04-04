b3b3dd6116ce7f03cb597ee682911a69657f7861
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-04-04 09:56:22 -0400

    start adjust settings/icons for work setup

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,7 +1,7 @@
 require('bufferline').setup({
   options = {
     mode = "tabs",
-    -- separator_style = 'slope',
+    separator_style = '|',
     always_show_bufferline = true,
     show_buffer_close_icons = true,
     show_close_icon = true,

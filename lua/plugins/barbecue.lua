ae81719454f5d366b142b59f5aa86803ffbb0c2a
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-02-14 09:09:45 -0500

    .

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -137,33 +137,3 @@ return {
     },
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- require('barbecue').setup({
---   show_modified = true,
---   -- settings for work
---   -- symbols = {
---   --   modified = "*",
---   --   ellipsis = "...",
---   --   separator = "/",
---   -- },
---   -- colors = {
---   --   modified = "#f6c177",
---   --   ellipsis = "#31748f",
---   --   separator = "#ebbcba",
---   -- },
---   -- kinds = nil
---   -- settings for work
--- })

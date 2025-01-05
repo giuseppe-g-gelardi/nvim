a93ec500a92f405b787be13740767753847100af
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2025-01-05 02:20:17 -0500

    disable null-ls -- didnt seem to be doing anything??? lsp seems to be handing formatting...

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,53 +1,52 @@
-return {
-  'nvimtools/none-ls.nvim',
-  dependencies = { -- Automatically install LSPs to stdpath for neovim
-    'nvim-lua/plenary.nvim',
-    'nvimtools/none-ls-extras.nvim'
-  },
-  config = function()
-    local status, null_ls = pcall(require, "null-ls")
-    if (not status) then return end
-
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    local lsp_formatting = function(bufnr)
-      vim.lsp.buf.format({
-        filter = function(client)
-          return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-      })
-    end
-
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.stylua,
-        require("none-ls.diagnostics.eslint"),
-        require("none-ls.diagnostics.ruff"),
-        null_ls.builtins.diagnostics.mypy,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              lsp_formatting(bufnr)
-            end,
-          })
-        end
-      end
-    }
-
-    vim.api.nvim_create_user_command(
-      'DisableLspFormatting',
-      function()
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-      end,
-      { nargs = 0 }
-    )
-  end,
-}
+return {}
+-- return {
+--   'nvimtools/none-ls.nvim',
+--   dependencies = { -- Automatically install LSPs to stdpath for neovim
+--     'nvim-lua/plenary.nvim',
+--     'nvimtools/none-ls-extras.nvim'
+--   },
+--   config = function()
+--     local status, null_ls = pcall(require, "null-ls")
+--     if (not status) then return end
+--
+--     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+--     local lsp_formatting = function(bufnr)
+--       vim.lsp.buf.format({
+--         filter = function(client)
+--           return client.name == "null-ls"
+--         end,
+--         bufnr = bufnr,
+--       })
+--     end
+--
+--     null_ls.setup {
+--       sources = {
+--         null_ls.builtins.formatting.black,
+--         null_ls.builtins.formatting.prettierd,
+--         null_ls.builtins.formatting.stylua,
+--         require("none-ls.diagnostics.eslint"),
+--       },
+--       on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--           vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--           vim.api.nvim_create_autocmd("BufWritePre", {
+--             group = augroup,
+--             buffer = bufnr,
+--             callback = function()
+--               lsp_formatting(bufnr)
+--             end,
+--           })
+--         end
+--       end
+--     }
+--
+--     vim.api.nvim_create_user_command(
+--       'DisableLspFormatting',
+--       function()
+--         vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+--       end,
+--       { nargs = 0 }
+--     )
+--   end,
+-- }

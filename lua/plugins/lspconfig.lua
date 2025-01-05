60f4a4c6adc2724c843505d92178dfe54f7dbc7d
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2025-01-05 01:29:51 -0500

    start replace cmp with blink --testing

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -79,7 +79,9 @@ return {
     end
 
     -- Set up completion using nvim_cmp with LSP source
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     capabilities.textDocument.completion.completionItem.snippetSupport = false
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -70,7 +70,7 @@ return {
       end, { desc = '[L]ocal [S]earch' }),
 
       vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[S]earch [F]iles' }),
-      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Git Files' }),
+      -- vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Git Files' }),
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' }),
       -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' }),
       vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[S]earch by [G]rep' }),

faa0d803708ce354e6484dd7feaec8999d86fd5c
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2026-02-05 09:59:36 -0500

    update rainbow brackets

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -3,6 +3,14 @@ return {
   config = function()
     local rainbow_delimiters = require('rainbow-delimiters')
 
+    -- Define custom colors matching VSCode ayu mirage bracket pair colorization
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterYellow', { fg = '#FFD700' })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterViolet', { fg = '#DA70D6' })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterBlue', { fg = '#179fff' })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterOrange', { fg = '#ffb454' })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterGreen', { fg = '#a0d96c' })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterCyan', { fg = '#5ccfe6' })
+
     vim.g.rainbow_delimiters = {
       strategy = {
         [''] = rainbow_delimiters.strategy['global'],
@@ -17,12 +25,11 @@ return {
         lua = 210,
       },
       highlight = {
-        'RainbowDelimiterRed',
         'RainbowDelimiterYellow',
+        'RainbowDelimiterViolet',
         'RainbowDelimiterBlue',
         'RainbowDelimiterOrange',
         'RainbowDelimiterGreen',
-        'RainbowDelimiterViolet',
         'RainbowDelimiterCyan',
       },
     }
